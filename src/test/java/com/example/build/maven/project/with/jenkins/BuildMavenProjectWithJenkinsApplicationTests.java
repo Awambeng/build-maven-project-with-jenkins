eab8851808711c428d100428d4d53eaf328a0291
Awambeng <awambengrodrick@gmail.com>
2024-08-01 08:41:47 +0100

    initial commit

---
+++ src/test/java/com/example/build/maven/project/with/jenkins/BuildMavenProjectWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.build.maven.project.with.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildMavenProjectWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
